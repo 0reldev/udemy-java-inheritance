357d339b04f9d7fbde4daa40613ef0b7ce3e781c
Aurélien Lonni <alonni@norsys.fr>
2023-04-24 17:28:00 +0200

    Lesson about inheritance part 2. Overridden methods examples.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,6 +6,11 @@ public class Main {
 
         Dog dog = new Dog();
         doAnimalStuff(dog, "fast");
+
+        Dog yorkie = new Dog("Yorkie", 15);
+        doAnimalStuff(yorkie, "fast");
+        Dog retriever = new Dog("Labrador Retriever", 65, "Floppy", "Swimmer");
+        doAnimalStuff(retriever, "slow");
     }
 
     public static void doAnimalStuff(Animal animal, String speed) {
